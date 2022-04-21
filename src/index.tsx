667bd090d2ff068790d80287cc29bfb074ed217a
fellipeutaka <fellipeutaka@gmail.com>
2022-04-21 20:30:52 -0300

    * feat(theme): theme config

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider resetCSS>
+    <ChakraProvider theme={theme} resetCSS>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
